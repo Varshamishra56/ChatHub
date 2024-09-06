845fc6443e94983f6071a674d198a763c3ad013b
Varshamishra56 <vm0222912@gmail.com>
2024-09-06 16:29:23 +0530

    name improved

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -34,7 +34,7 @@ const HomePage = () => {
         borderWidth={"1px"}
       >
         <Text fontSize={"4xl"} fontFamily={"cursive"}>
-          Talk-a-Tive
+          ChatHub
         </Text>
       </Box>
       <Box
